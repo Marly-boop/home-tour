6f8eb2a32ca9d654c7c197b1b12d132698a10dc3
marly teixeira <teixeiramg.marly@gmail.com>
2021-09-27 14:42:53 +0200

    adding card category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'houses/index'
   #get 'houses/index'
-  root to:"categories#index"
+  root to:"houses#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
